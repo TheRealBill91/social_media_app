b74e4d185461204e8c42c318444fb67e7331fca9
Billy Cummings <bc35786@gmail.com>
2024-09-17 18:41:18 -0500

    refactor: modify ResendConfirmationEmailBtn to use StatusButton component

--- frontend/app/components/ui/ResendConfirmationEmailBtn.tsx
+++ frontend/app/components/ui/ResendConfirmationEmailBtn.tsx
@@ -1,21 +1,30 @@
+import { useIsPending } from "~/utils/misc";
+import { StatusButton } from "./StatusButton";
+
 interface ResendEmailBtn {
-  submitting: boolean;
   emailMissing: boolean;
+  state: "submitting" | "loading" | "idle";
+  data?: { status: "error" } | { status: null } | undefined;
 }
 
 export function ResendConfirmationEmailBtn({
-  submitting,
   emailMissing,
+  state,
+  data,
 }: ResendEmailBtn) {
+  const isPending = useIsPending();
+
   return (
-    <button
-      aria-disabled={submitting}
-      disabled={emailMissing}
-      type="submit"
-      className="inline-flex h-10 w-full cursor-pointer items-center justify-center rounded-md bg-gray-800 px-4 py-2 text-center text-primary-foreground text-white outline-0 transition-all  hover:bg-gray-800/90  focus-visible:ring focus-visible:ring-gray-500 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-600 disabled:focus:ring-gray-400 aria-disabled:cursor-not-allowed aria-disabled:bg-gray-300 aria-disabled:text-gray-600 aria-disabled:focus:ring-gray-400"
-    >
-      {" "}
-      Resend Confirmation Email
-    </button>
+    <div className="flex w-full flex-1 items-center justify-between gap-6">
+      <StatusButton
+        aria-disabled={isPending}
+        className="w-full flex-1 cursor-pointer gap-4 bg-gray-800 px-3 py-[10px] text-sm capitalize hover:bg-gray-900 disabled:cursor-not-allowed"
+        status={state === "submitting" ? "pending" : data?.status ?? "idle"}
+        type="submit"
+        disabled={isPending || emailMissing}
+      >
+        Resend Confirmation Email
+      </StatusButton>
+    </div>
   );
 }
