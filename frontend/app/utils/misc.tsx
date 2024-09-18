ffd97456c6d02a79ffb1618b943d2c49cb255265
Billy Cummings <bc35786@gmail.com>
2024-09-17 19:15:58 -0500

    Create useIsPending and combineHeaders utility functions

--- frontend/app/utils/misc.tsx
+++ frontend/app/utils/misc.tsx
@@ -1,3 +1,4 @@
+import { useFormAction, useNavigation } from "@remix-run/react";
 import { clsx, type ClassValue } from "clsx";
 
 import { twMerge } from "tailwind-merge";
@@ -18,4 +19,52 @@ import { twMerge } from "tailwind-merge";
  */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+/**
+ * Returns true if the current navigation is submitting the current route's
+ * form. Defaults to the current route's form action and method POST.
+ *
+ * Defaults state to 'non-idle'
+ *
+ * NOTE: the default formAction will include query params, but the
+ * navigation.formAction will not, so don't use the default formAction if you
+ * want to know if a form is submitting without specific query params.
+ */
+export function useIsPending({
+  formAction,
+  formMethod = "POST",
+  state = "non-idle",
+}: {
+  formAction?: string;
+  formMethod?: "POST" | "GET" | "PUT" | "PATCH" | "DELETE";
+  state?: "submitting" | "loading" | "non-idle";
+} = {}) {
+  const contextualFormAction = useFormAction();
+  const navigation = useNavigation();
+  const isPendingState =
+    state === "non-idle"
+      ? navigation.state !== "idle"
+      : navigation.state === state;
+  return (
+    isPendingState &&
+    navigation.formAction === (formAction ?? contextualFormAction) &&
+    navigation.formMethod === formMethod
+  );
+}
+
+/**
+ * Combine multiple header objects into one (uses append so headers are not overridden)
+ */
+export function combineHeaders(
+  ...headers: Array<ResponseInit["headers"] | null | undefined>
+) {
+  const combined = new Headers();
+  for (const header of headers) {
+    if (!header) continue;
+    for (const [key, value] of new Headers(header).entries()) {
+      combined.append(key, value);
+    }
+  }
+  return combined;
 }
