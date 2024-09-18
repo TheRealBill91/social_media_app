ca0ef3e8e288bca67e91e2ddd847052004e68f53
Billy Cummings <bc35786@gmail.com>
2024-09-17 19:39:56 -0500

    Modify styles in DropDownMenu & Sheet components

--- frontend/app/components/ui/Sheet.tsx
+++ frontend/app/components/ui/Sheet.tsx
@@ -63,7 +63,7 @@ const SheetContent = React.forwardRef<
       {...props}
     >
       {children}
-      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm p-1 opacity-70 outline-0 ring-offset-background transition-all hover:opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-black disabled:pointer-events-none data-[state=open]:bg-secondary md:p-2 md:hover:bg-gray-200/80">
         <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
